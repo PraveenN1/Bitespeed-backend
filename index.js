058150b53dc2ae5755d849b18216263adcea712d
PraveenN1 <105967145+PraveenN1@users.noreply.github.com>
2025-07-18 17:44:42 +0530

    Initial commit:backend

---
+++ index.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app=express();
+const PORT=process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(
+        `Server running on PORT ${PORT}`
+    )
+});
